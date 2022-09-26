3d0606cabcb8ba42eba9e0688d14192cb4d8aeae
Andrei <102610731+AndreiKoenich@users.noreply.github.com>
2022-09-25 21:22:59 -0300

    Update Resolução.sql

--- Aula Presencial 4 – Praticando subconsultas/Resolução.sql
+++ Aula Presencial 4 – Praticando subconsultas/Resolução.sql
@@ -110,9 +110,9 @@ order by nome_medico
 
 select distinct nomem as nome_medico
 from medico
-where crm not in (select crm
-		  from consulta natural join paciente
-		  where idadep > 12)
+where idadep <= 12 and crm not in (select crm
+		                       from consulta natural join paciente
+		                       where idadep > 12)
 order by nome_medico
 				  
 -- d. atenderam crianças e adultos (adulto >=21)
